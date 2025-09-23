d9402cf1a56e386eb0c94f7279d99996d3950324
interiorsofcalifornia <interiorsofcalifornia@users.noreply.github.com>
2025-09-23 00:18:27 -0700

    Fix metadata type annotation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Interiors of California",
   description: "We will floor you! Premium interior remodeling in Northern California.",
   openGraph: {
